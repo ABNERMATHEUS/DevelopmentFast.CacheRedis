14a632ee74315dd14150f2f79db11e3962ae4e58
Abner Matheus <abnerm80@gmail.com>
2021-12-17 14:30:41 -0300

    Adicionar arquivos de projeto.

---
+++ DevelopmentFast.Cache.Redis/Domain/Interfaces/Repository/IBaseRedisRepositoryDF.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevelopmentFast.Cache.Redis.Domain.Interfaces.Repository
+{
+    public interface IBaseRedisRepositoryDF
+    {
+        T? Get<T>(string key);
+        Task<T?> GetAsync<T>(string key);
+
+        void CreateOrUpdate<T>(string key, T obj, TimeSpan timeSpan);
+        Task CreateOrUpdateAsync<T>(string key, T obj, TimeSpan timeSpan);
+    }
+}
---
+++ DevelopmentFast.Cache.Redis/Repository/BaseRedisRepositoryDF.cs
@@ -0,0 +1,46 @@
+﻿using DevelopmentFast.Cache.Redis.Domain.Interfaces.Repository;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text;
+using System.Text.Json;
+
+namespace DevelopmentFast.Cache.Redis.Repository
+{
+    public class BaseRedisRepositoryDF : IBaseRedisRepositoryDF
+    {
+        private readonly IDistributedCache _rd_context;
+
+        public BaseRedisRepositoryDF(IDistributedCache rd_context)
+        {
+            _rd_context = rd_context;
+        }
+
+        public void CreateOrUpdate<T>(string key, T obj, TimeSpan timeSpan)
+        {
+            DistributedCacheEntryOptions options = new DistributedCacheEntryOptions();
+            options.SetSlidingExpiration(timeSpan);
+            _rd_context.SetString(key, JsonSerializer.Serialize(obj), options);
+        }
+
+        public async Task CreateOrUpdateAsync<T>(string key, T obj, TimeSpan timeSpan)
+        {
+            var options = new DistributedCacheEntryOptions();
+            options.SetSlidingExpiration(timeSpan);
+            await _rd_context.SetStringAsync(key, JsonSerializer.Serialize(obj), options);
+        }
+
+        public T? Get<T>(string key)
+        {
+            return JsonSerializer.Deserialize<T>(_rd_context.GetString(key));
+        }
+
+        public async Task<T?> GetAsync<T>(string key)
+        {
+            var obj = await _rd_context.GetStringAsync(key);
+            if (obj != null)
+            {
+                return JsonSerializer.Deserialize<T>(obj);
+            }
+            return default;
+        }
+    }
+}
